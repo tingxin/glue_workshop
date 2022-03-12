1903e09f2c3e1dcce12decd29a4b2b6f26742467
EC2 Default User <ec2-user@ip-172-31-43-238.cn-northwest-1.compute.internal>
2022-03-12 05:28:21 +0000

    update

--- temp.py
+++ temp.py
@@ -1,9 +1,10 @@
 from secret import get_redshift_secret
 import json
 
-redshift_info = get_redshift_secret()
-redshift_info = get_redshift_secret()
+secret_name = "dev/mall/redshift"
+region_name = "cn-northwest-1"
+redshift_info = get_redshift_secret(secret_name, region_name)
 redshift_host = redshift_info['host']
 redshift_port = redshift_info['port']
 redshift_jdbc = f"jdbc:redshift://{redshift_host}:{redshift_port}/dev"
-print(redshift_jdbc)
+print(redshift_info)
